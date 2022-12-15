54151a9d595a0b59f601fae37724d4ee9f81a0b5
nissmogt <k.mehrabiani@gmail.com>
2022-12-14 17:17:20 -0800

    added confirmation and directory message

--- TranscribeYT/yt_transcription.py
+++ TranscribeYT/yt_transcription.py
@@ -98,4 +98,5 @@ if __name__ == '__main__':
         url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
     transcribe_audio(str(url))
+    print("Done! Transcription saved to {} directory.".format(os.path.join(os.getcwd(), 'transcriptions')))
 
